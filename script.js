11537e0aa10b56d3dd1c79f773ef3e4c385ffad4
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-20 19:52:55 +0000

    add color code function to display timeblock

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 var currentDay = dayjs().format('dddd, MMMM YY');
 $('#currentDay').text(currentDay);
 
+//Color-code each timeblock based on past, present, and future 
+function colorCodeHours() {
+    var currentTime = dayjs().hour();
+
+    $(".hour ").each(function () {
+        var idValue = $(this).attr("id");
+
+        var idHourValue = parseInt(idValue.split("-")[1]);
+        console.log(idHourValue);
+
+        if (idHourValue < currentTime) {
+            $('.description').addClass('past');
+        } else if (idHourValue === currentTime) {
+            $('.description').addClass('present');
+        } else {
+            $('.description').addClass('future');
+        }
+    });
+    console.log(currentTime);
+};
+colorCodeHours();
+
