92b5c9df15cc703f997173cb419669c2a869893b
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-21 20:09:37 +0000

    add new day reset function

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
-// Display current day
-var currentDay = dayjs().format('dddd, MMMM D');
-$('#currentDay').text(currentDay);
-console.log('currentDay: ' + currentDay);
+$(document).ready(function () {
+    //Display current day
+    var currentDay = dayjs().format('dddd, MMMM D');
+    $('#currentDay').text(currentDay); 
 
-var timeblock = $(".time-block");
+     // Retrieve saved current day from localStorage
+    var savedCurrentDay = localStorage.getItem('savedCurrentDay');
 
-//Color-code each timeblock based on past, present, and future 
-function colorCodeHours() {
-    var currentTime = dayjs().hour();
+    var timeblock = $(".time-block");
 
-    timeblock.each(function () {
-        var idValue = $(this).attr("id");
-        var idHourValue = parseInt(idValue.split("-")[1]);
-      
-        if (idHourValue < currentTime) {
-            $(this).addClass('past');
-        } else if (idHourValue === currentTime) {
-            $(this).removeClass('past');
-            $(this).addClass('present');
+    //Color-code each timeblock 
+    function colorCodeHours() {
+        var currentTime = dayjs().hour();
+        $('#feedback').text("Add your event below!")
+        
+        timeblock.each(function () {
+            var idValue = $(this).attr("id");
+            var idHourValue = parseInt(idValue.split("-")[1]);
+
+            //Color-code for past hour
+            if (idHourValue < currentTime) {
+                $(this).addClass('past');
+
+                //Color-code for present hour
+            } else if (idHourValue === currentTime) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+
+                //Color-code for future hour
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        });
+
+    };
+
+    colorCodeHours();
+
+    //Save each input schedule individually in localStorage. 
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+
+        //User input hourly Schedule
+        var hourlyScheduleInput = $(this).parent().find('.description').val();
+        var timeKeyOfInput = $(this).parent().find('.hour').text();
+
+        //Check the input is vaild before save to localStorage.
+        if (!hourlyScheduleInput) {
+            return;
         } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass('future');
-        }
+            localStorage.setItem(timeKeyOfInput, hourlyScheduleInput);
+            $('#feedback').text("Event added to LocalStorage!")
+            localStorage.setItem('savedCurrentDay', currentDay);
+        };
     });
-    console.log('currentTime: '+ currentTime);
-};
 
-colorCodeHours();
+    //Retrieve hourlySchedule from local storage
+    function displaySchedule() {
+        timeblock.each(function () {
+            //Get key
+            var timeKey = $(this).find('.hour').text();
 
+            //Get Vaue
+            var hourlySchedule = localStorage.getItem(timeKey);
+ 
+            //Display on the page
+            $(this).find('.description').val(hourlySchedule);
+        });
+    }
 
-//Save each input schedule individually in localStorage. 
-$(".saveBtn").click(function (event) {
-    event.preventDefault();
+    displaySchedule();
 
-    //User input hourly Schedule
-    var hourlyScheduleInput = $(this).parent().find('.description').val();
-    var timeKeyOfInput = $(this).parent().find('.hour').text();
+    // Check if a new day has started
+    if (currentDay !== savedCurrentDay) {
+        NewDayReset();
+    };
 
-    //Check the input is vaild before save to localStorage.
-    if (!hourlyScheduleInput) {
-        return;
-    } else {
-        localStorage.setItem(timeKeyOfInput, hourlyScheduleInput);
-        $('#feedback').text("Event added to LocalStorage!")   
-    }; 
-       
+    function NewDayReset() {
+        //Clear page input
+        $("textarea").val('');
+        //Clear the storage 
+        localStorage.clear();
+
+    }
+
 });
-
-//Retrieve hourlySchedule from local storage
-function displaySchedule() {
-    timeblock.each(function () {
-        //Get key
-        var timeKey = $(this).find('.hour').text();
-        console.log(timeKey);
-        //Get Vaue
-        var hourlySchedule = localStorage.getItem(timeKey);
-        console.log(hourlySchedule);
-        //Display on the page
-        $(this).find('.description').val(hourlySchedule);
-    });
-}
-displaySchedule();
-
