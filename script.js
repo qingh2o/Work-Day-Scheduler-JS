e0c6a7b259657a2faf23e1a81dea5033817c2883
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-21 16:40:10 +0000

    add function retrieve data from local storage

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ $('#currentDay').text(currentDay);
 console.log('currentDay: ' + currentDay);
 
 var timeblock = $(".time-block");
-var userInput = $(".description");
-
 
 //Color-code each timeblock based on past, present, and future 
 function colorCodeHours() {
@@ -14,8 +12,7 @@ function colorCodeHours() {
     timeblock.each(function () {
         var idValue = $(this).attr("id");
         var idHourValue = parseInt(idValue.split("-")[1]);
-        console.log(idHourValue);
-
+      
         if (idHourValue < currentTime) {
             $(this).addClass('past');
         } else if (idHourValue === currentTime) {
@@ -31,22 +28,38 @@ function colorCodeHours() {
 };
 
 colorCodeHours();
-var hourlySchedule = "";
+
 
 //Save each input schedule individually in localStorage. 
 $(".saveBtn").click(function (event) {
     event.preventDefault();
-    hourlySchedule = $(this).siblings().next().val();
-    var timeKey = $(this).siblings().text();
+
+    //User input hourly Schedule
+    var hourlyScheduleInput = $(this).parent().find('.description').val();
+    var timeKeyOfInput = $(this).parent().find('.hour').text();
 
     //Check the input is vaild before save to localStorage.
-    if (!hourlySchedule) {
+    if (!hourlyScheduleInput) {
         return;
     } else {
-        localStorage.setItem(timeKey, hourlySchedule);
-    };   
+        localStorage.setItem(timeKeyOfInput, hourlyScheduleInput);
+        $('#feedback').text("Event added to LocalStorage!")   
+    }; 
+       
 });
 
-
-
+//Retrieve hourlySchedule from local storage
+function displaySchedule() {
+    timeblock.each(function () {
+        //Get key
+        var timeKey = $(this).find('.hour').text();
+        console.log(timeKey);
+        //Get Vaue
+        var hourlySchedule = localStorage.getItem(timeKey);
+        console.log(hourlySchedule);
+        //Display on the page
+        $(this).find('.description').val(hourlySchedule);
+    });
+}
+displaySchedule();
 
