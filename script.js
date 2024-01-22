768b6c2dc16ffdbfb5d6cd20eed1848af0d47fe7
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-01-22 09:40:31 +0000

    edit comment

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
     //Display current day
-    var currentDay = dayjs().format('dddd, MMMM D');
+    var currentDay = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDay').text(currentDay);
 
-    // Retrieve saved current day from localStorage
+    //Retrieve saved current day from localStorage for comparing new day
     var savedCurrentDay = localStorage.getItem('savedCurrentDay');
 
     var timeblock = $(".time-block");
 
-    //Color-code each timeblock 
+    //Display color-code timeblocks 
     function colorCodeHours() {
         var currentTime = dayjs().hour();
-
         timeblock.each(function () {
+
+            //Use the number in id to compare with the current hour
             var idValue = $(this).attr("id");
             var idHourValue = parseInt(idValue.split("-")[1]);
 
@@ -22,16 +23,13 @@ $(document).ready(function () {
 
                 //Color-code for present hour
             } else if (idHourValue === currentTime) {
-                $(this).removeClass('past');
-                $(this).addClass('present');
+                $(this).removeClass('past').addClass('present');
 
                 //Color-code for future hour
             } else {
-                $(this).removeClass('past present');
-                $(this).addClass('future');
+                $(this).removeClass('past present').addClass('future');
             }
         });
-
     };
 
     colorCodeHours();
@@ -39,7 +37,7 @@ $(document).ready(function () {
     //Save each input schedule individually in localStorage. 
     $(".saveBtn").on('click', function () {
 
-        //User input hourly Schedule
+        //Set key and value for localStorage when user inputs hourly Schedule
         var hourlyScheduleInput = $(this).parent().find('.description').val();
         var timeKeyOfInput = $(this).parent().find('.hour').text();
 
@@ -47,14 +45,18 @@ $(document).ready(function () {
         if (!hourlyScheduleInput) {
             return;
         } else {
+            //Save current date to compare with new day
             localStorage.setItem('savedCurrentDay', currentDay);
+
+            //Save key and value for localStorage 
             localStorage.setItem(timeKeyOfInput, hourlyScheduleInput);
-            //Use Toastr for notification
+
+            //Use Toastr for notification after user clicks the save button
             toastr.success("Event added to Local storage!");
         };
     });
 
-    //Retrieve hourlySchedule from local storage
+    //Retrieve hourlySchedule from localStorage
     function displaySchedule() {
         timeblock.each(function () {
             //Get key
@@ -66,18 +68,17 @@ $(document).ready(function () {
             //Display on the page
             $(this).find('.description').val(hourlySchedule);
         });
-    }
+    };
 
     displaySchedule();
 
-    // Check if a new day has started
+    //Check if a new day has started (reserve localStorage for new day input replacement)
     if (currentDay !== savedCurrentDay) {
         NewDayReset();
     };
 
+    //Clear page input 
     function NewDayReset() {
-        //Clear page input
         $("textarea").val('');
-    }
-
+    };
 });
